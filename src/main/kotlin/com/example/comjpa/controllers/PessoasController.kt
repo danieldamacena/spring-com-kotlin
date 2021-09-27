a1ffd22a783fa2cb2abe870eb07edbbbcd0fad24
danieldamacena-ma <danfelipeb@gmail.com>
2021-09-27 11:15:33 -0300

    adicionando post

--- src/main/kotlin/com/example/comjpa/controllers/PessoasController.kt
+++ src/main/kotlin/com/example/comjpa/controllers/PessoasController.kt
@@ -1,6 +1,7 @@
 package com.example.comjpa.controllers
 
 
+import com.example.comjpa.models.Pessoas
 import com.example.comjpa.repository.PessoasRepo
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
@@ -9,6 +10,10 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class PessoasController(private val repo: PessoasRepo) {
+    @Autowired
     @GetMapping("/pessoas")
-    fun lista() = repo.findAll()
+    fun lista(): List<Pessoas> = repo.findAll()
+
+    fun cria(pessoa: Pessoas): Pessoas = repo.save(pessoa)
+
 }
