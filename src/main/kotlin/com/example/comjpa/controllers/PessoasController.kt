185b5113a1cbb3021c066097f4259b874d57371e
danieldamacena-ma <danfelipeb@gmail.com>
2021-09-29 12:18:05 -0300

    mostraPessoaById implementado

--- src/main/kotlin/com/example/comjpa/controllers/PessoasController.kt
+++ src/main/kotlin/com/example/comjpa/controllers/PessoasController.kt
@@ -4,6 +4,7 @@ package com.example.comjpa.controllers
 import com.example.comjpa.models.Pessoas
 import com.example.comjpa.service.PessoasService
 import org.springframework.web.bind.annotation.*
+import java.util.*
 
 
 @RestController
@@ -11,6 +12,9 @@ class PessoasController(private val service: PessoasService) {
 
     @GetMapping("/pessoas")
     fun listaPessoas(): List<Pessoas> = service.listaPessoas()
+
+    @GetMapping("/pessoas/{pessoaId}")
+    fun mostraPessoaById(@PathVariable("pessoaId") pessoaId: Int): Pessoas = service.getById(pessoaId)
 
     @PostMapping("/post")
     fun criaPessoa(@RequestBody pessoa: Pessoas): Pessoas = service.criaPessoa(pessoa)
--- src/main/kotlin/com/example/comjpa/service/pessoasService.kt
+++ src/main/kotlin/com/example/comjpa/service/pessoasService.kt
@@ -5,12 +5,17 @@ import com.example.comjpa.repository.PessoasRepo
 import javassist.NotFoundException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class PessoasService(private val repository: PessoasRepo) {
     @Autowired
 
     fun listaPessoas(): List<Pessoas> = repository.findAll()
+
+    fun getById(pessoaId: Int): Pessoas = repository.findById(pessoaId)
+        .orElseThrow {NotFoundException("não há id correspondente")}
+
 
     fun criaPessoa(pessoa: Pessoas): Pessoas = repository.save(pessoa)
 
