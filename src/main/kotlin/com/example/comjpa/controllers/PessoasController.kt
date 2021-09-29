a3ac6460a6c02a2c4c0d23cbb550218a32a39a36
danieldamacena-ma <danfelipeb@gmail.com>
2021-09-29 10:05:29 -0300

    get, post e delete no pacote service

--- src/main/kotlin/com/example/comjpa/controllers/PessoasController.kt
+++ src/main/kotlin/com/example/comjpa/controllers/PessoasController.kt
@@ -2,17 +2,12 @@ package com.example.comjpa.controllers
 
 
 import com.example.comjpa.models.Pessoas
-import com.example.comjpa.service.pessoasService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import com.example.comjpa.service.PessoasService
+import org.springframework.web.bind.annotation.*
 
 
 @RestController
-class PessoasController(
-    private val service: pessoasService
-) {
+class PessoasController(private val service: PessoasService) {
 
     @GetMapping("/pessoas")
     fun listaPessoas(): List<Pessoas> = service.listaPessoas()
@@ -20,11 +15,6 @@ class PessoasController(
     @PostMapping("/post")
     fun criaPessoa(@RequestBody pessoa: Pessoas): Pessoas = service.criaPessoa(pessoa)
 
-/*
     @DeleteMapping("/delete/{pessoaId}")
-    fun deletaPessoa(@PathVariable("pessoaId") pessoaId: Int) {
-        if (repo.existsById(pessoaId)) {
-            repo.deleteById(pessoaId)
-        }
-    } */
+    fun deletaPessoa(@PathVariable("pessoaId") pessoaId: Int) = service.deletaPessoa(pessoaId)
 }
--- src/main/kotlin/com/example/comjpa/service/pessoasService.kt
+++ src/main/kotlin/com/example/comjpa/service/pessoasService.kt
@@ -2,15 +2,23 @@ package com.example.comjpa.service
 
 import com.example.comjpa.models.Pessoas
 import com.example.comjpa.repository.PessoasRepo
+import javassist.NotFoundException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class pessoasService(private val repository: PessoasRepo) {
+class PessoasService(private val repository: PessoasRepo) {
     @Autowired
 
     fun listaPessoas(): List<Pessoas> = repository.findAll()
 
     fun criaPessoa(pessoa: Pessoas): Pessoas = repository.save(pessoa)
 
+    fun deletaPessoa(pessoaId: Int) {
+        if (repository.existsById(pessoaId)) {
+            repository.deleteById(pessoaId)
+        } else {
+            throw NotFoundException("Não existe id correspondente no banco de dados")
+        }
+    }
 }
